81ef5ce58d7f6588103fe160fb8fdbf6180c20e4
Mike Vercoelen <mike.vercoelen@gmail.com>
2019-01-09 13:46:31 +0100

    Added a docker-compose file setup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM node:10.15.0-alpine
-
-WORKDIR /app
-COPY . .
-
-RUN npm install --prod
-
 ENV NODE_ENV=production
+WORKDIR /usr/app
+
+COPY ./package.json ./
+RUN npm install && \
+    npm cache clean --force
+
 EXPOSE 3000
-CMD ["node", "./src/index.js"]
+
+CMD ["npm", "run", "start-production"]
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,20 @@
 const winston = require('winston')
 
-// TODO: what kind of transports do we want to have in production? Maybe Logentries?
-
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json()
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  ]
 })
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }))
-}
+// TODO: what kind of transports do we want to have in production? Maybe Logentries?
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     format: winston.format.simple()
+//   }))
+// }
 
 module.exports = logger
